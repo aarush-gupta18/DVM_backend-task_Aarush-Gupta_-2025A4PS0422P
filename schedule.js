01b6f7ab6959fb6a0de338ff72807b4b50038493
aarush-gupta18 <2007aarush@gmail.com>
2026-03-29 18:15:48 +0530

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -8,7 +8,7 @@ const events = [
   { id: 7, name: "CaseQuesta", category: "Tech", day: 1, time: "15:00", venue: "FD-1,1203", registrations: 213 },
   { id: 8, name: "RoboSoccer", category: "Tech", day: 1, time: "17:00", venue: "Vk Lawns", registrations: 678 },
   { id: 9, name: "InaugParty", category: "Party", day: 0, time: "23:00", venue: "Sac", registrations: 475 },
-  { id: 10, name: "SpeedDating♥️", category: "Party", day: 2, time: "18:00", venue: "Sac", registrations: 1127 },
+  { id: 10, name: "SpeedDating", category: "Party", day: 2, time: "18:00", venue: "Sac", registrations: 1127 },
   { id: 11, name: "StreetDance", category: "Dance", day: 2, time: "10:00", venue: "Rotunda", registrations: 87 }
   
 
